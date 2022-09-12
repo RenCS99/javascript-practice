da0550bf29fb856fa95b41eba835c3fe96d2f1a0
RenCS99 <rcuevasalas99@gmail.com>
2022-09-12 11:59:45 -0700

    Added different types of variables and console log

--- js/script.js
+++ js/script.js
@@ -6,3 +6,12 @@
 console.log("My name is Rene Cueva Salas.");
 console.log("Alfredo Pasta is one of my favorite foods.");
 console.log("Sushi is also one of my favorite foods.");
+
+const word = "Supercalifragilisticexpialidocious";
+console.log(word.length);
+const num = 720;
+console.log(num);
+console.log(typeof num);
+const bigInt = BigInt(59);
+console.log(bigInt);
+console.log(typeof bigInt);
