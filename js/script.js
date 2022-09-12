fcb7b1c991ec3db21851abbabb1e2dc94216a77f
RenCS99 <rcuevasalas99@gmail.com>
2022-09-12 11:13:51 -0700

    Added comments and console log sentences

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Single Comment Example
+/* Multi-Example
+ Commment
+ Example */
+
+console.log("My name is Rene Cueva Salas.");
+console.log("Alfredo Pasta is one of my favorite foods.");
+console.log("Sushi is also one of my favorite foods.");
